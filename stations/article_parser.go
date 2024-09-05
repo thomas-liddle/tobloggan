fd839051bba1c38d73cfef58d73abb494c824320
Michael Whatcott <mike@smarty.com>
2024-09-05 15:24:35 -0600

    Implement article parser.

---
+++ stations/article_parser.go
@@ -0,0 +1,42 @@
+package stations
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/mdwhatcott/tobloggan/contracts"
+)
+
+type ArticleParser struct {
+	md contracts.Markdown
+}
+
+func NewArticleParser(md contracts.Markdown) *ArticleParser {
+	return &ArticleParser{md: md}
+}
+
+func (this *ArticleParser) Do(input any, output func(any)) {
+	switch input := input.(type) {
+	case contracts.SourceFile:
+		front, body, divided := bytes.Cut(input, []byte("\n+++\n"))
+		if !divided {
+			output(fmt.Errorf("%w (missing divider): %s", contracts.ErrMalformedSource, input))
+			return
+		}
+		var source contracts.Article
+		err := json.Unmarshal(front, &source)
+		if err != nil {
+			output(fmt.Errorf("%w (%w): %s", contracts.ErrMalformedSource, err, input))
+			return
+		}
+		source.Body, err = this.md.Convert(string(bytes.TrimSpace(body)))
+		if err != nil {
+			output(fmt.Errorf("%w (%w): %s", contracts.ErrMalformedSource, err, input))
+			return
+		}
+		output(source)
+	default:
+		output(input)
+	}
+}
---
+++ stations/article_parser_test.go
@@ -0,0 +1,74 @@
+package stations
+
+import (
+	"testing"
+	"time"
+
+	"github.com/mdwhatcott/tobloggan/contracts"
+	"github.com/smarty/assertions/should"
+	"github.com/smarty/gunit"
+)
+
+func TestArticleParserFixture(t *testing.T) {
+	gunit.Run(new(ArticleParserFixture), t)
+}
+
+type ArticleParserFixture struct {
+	*gunit.Fixture
+	StationFixture
+	parser      *ArticleParser
+	markdownErr error
+}
+
+func (this *ArticleParserFixture) Convert(content string) (string, error) {
+	return content + " CONVERTED", this.markdownErr
+}
+
+func (this *ArticleParserFixture) Setup() {
+	this.parser = NewArticleParser(this)
+}
+
+func (this *ArticleParserFixture) TestUnhandledTypeEmitted() {
+	this.parser.Do("wrong-type", this.Output)
+	this.So(this.outputs, should.Equal, []any{"wrong-type"})
+}
+func (this *ArticleParserFixture) TestArticleMetaAndContentReadFromDiskAndEmitted() {
+	this.parser.Do(contracts.SourceFile(article1Content), this.Output)
+	this.So(this.outputs, should.Equal, []any{
+		contracts.Article{
+			Slug:  "/article/1",
+			Title: "Article 1",
+			Date:  time.Date(2024, time.September, 4, 0, 0, 0, 0, time.UTC),
+			Body:  "The contents of article 1. CONVERTED",
+		},
+	})
+}
+func (this *ArticleParserFixture) TestMissingDivider() {
+	this.parser.Do(contracts.SourceFile("{} Content without separator"), this.Output)
+	if this.So(this.outputs, should.HaveLength, 1) {
+		this.So(this.outputs[0], should.Wrap, contracts.ErrMalformedSource)
+	}
+}
+func (this *ArticleParserFixture) TestMalformedMetadata() {
+	this.parser.Do(contracts.SourceFile("{bad-json}\n+++\nContent"), this.Output)
+	if this.So(this.outputs, should.HaveLength, 1) {
+		this.So(this.outputs[0], should.Wrap, contracts.ErrMalformedSource)
+	}
+}
+func (this *ArticleParserFixture) TestInvalidMarkdown() {
+	this.markdownErr = boink
+	this.parser.Do(contracts.SourceFile(article1Content), this.Output)
+	if this.So(this.outputs, should.HaveLength, 1) {
+		this.So(this.outputs[0], should.Wrap, contracts.ErrMalformedSource)
+	}
+}
+
+const article1Content = `{
+	"date": "2024-09-04T00:00:00Z",
+	"slug": "/article/1",
+	"title": "Article 1"
+}
+
++++
+
+The contents of article 1.`
