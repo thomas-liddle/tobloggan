7bb310ce6ea6889a70fe4ba60e50ceadcee105bb
Michael Whatcott <mike@smarty.com>
2024-09-25 14:47:38 -0600

    Merge type declarations.

--- code/contracts/pipeline.go
+++ code/contracts/pipeline.go
@@ -2,16 +2,18 @@ package contracts
 
 import "time"
 
-type SourceDirectory string
-type SourceFilePath string
-type SourceFile []byte
-type Article struct {
-	Slug  string    `json:"slug"`
-	Title string    `json:"title"`
-	Date  time.Time `json:"date"`
-	Body  string    `json:"-"`
-}
-type Page struct {
-	Path    string
-	Content string
-}
+type (
+	SourceDirectory string
+	SourceFilePath  string
+	SourceFile      []byte
+	Article         struct {
+		Slug  string    `json:"slug"`
+		Title string    `json:"title"`
+		Date  time.Time `json:"date"`
+		Body  string    `json:"-"`
+	}
+	Page struct {
+		Path    string
+		Content string
+	}
+)
