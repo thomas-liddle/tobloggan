a41252bd64e17388cdd6c8558402a163228a379b
Michael Whatcott <mike@smarty.com>
2024-09-26 11:13:55 -0600

    Remove hard-coded domain.

--- code/html/listing.go
+++ code/html/listing.go
@@ -8,7 +8,6 @@ const ListingTemplate = `<!doctype html>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Your Name Here">
         <meta name="description" content="description">
-        <link rel="canonical" href="https://your-domain-here.com">
         ` + CSS + `
     </head>
 
