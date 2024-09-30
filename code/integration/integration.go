19e73920d05c0c2fcf3eb26b0a8c2ede2c6335b4
Michael Whatcott <mike@smarty.com>
2024-09-30 12:02:17 -0600

    Restore SourceScanner station.

--- code/integration/integration.go
+++ code/integration/integration.go
@@ -31,7 +31,7 @@ func GenerateBlog(config Config) bool {
 		failure = new(atomic.Bool)
 		input   = make(chan any, 1)
 
-		scanner   = pipelines.Station(nil) // stations.NewSourceScanner(config.FileSystemReader)
+		scanner   = stations.NewSourceScanner(config.FileSystemReader)
 		reader    = pipelines.Station(nil) // stations.NewSourceReader(config.FileSystemReader)
 		parser    = pipelines.Station(nil) // stations.NewArticleParser()
 		validator = pipelines.Station(nil) // stations.NewArticleValidator() // OPTIONAL?
