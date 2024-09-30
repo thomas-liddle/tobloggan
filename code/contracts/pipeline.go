7a45330b24e301ff4e53849f1dab8fb64a50a914
Michael Whatcott <mike@smarty.com>
2024-09-30 13:41:31 -0600

    Document data model.

--- code/contracts/pipeline.go
+++ code/contracts/pipeline.go
@@ -2,19 +2,26 @@ package contracts
 
 import "time"
 
-type (
-	SourceDirectory string
-	SourceFilePath  string
-	SourceFile      []byte
-	Article         struct {
-		Draft bool      `json:"draft"`
-		Slug  string    `json:"slug"`
-		Title string    `json:"title"`
-		Date  time.Time `json:"date"`
-		Body  string    `json:"-"`
-	}
-	Page struct {
-		Path    string
-		Content string
-	}
-)
+// SourceDirectory is just a path representing a directory.
+type SourceDirectory string
+
+// SourceFilePath is just a path representing a content file
+type SourceFilePath string
+
+// SourceFile is the raw contents of a content file.
+type SourceFile []byte
+
+// Article represents the parsed data from a SourceFile.
+type Article struct {
+	Draft bool      `json:"draft"` // If true, don't publish this article.
+	Slug  string    `json:"slug"`  // The URL path of the article.
+	Title string    `json:"title"` // The <h1> of the article.
+	Date  time.Time `json:"date"`  // Affects sorting of articles.
+	Body  string    `json:"-"`     // Markdown, will become HTML.
+}
+
+// Page represents a rendered article, ready to write to disk.
+type Page struct {
+	Path    string
+	Content string
+}
