49914ca22f96b5e17bba814f3d30fb39c085a5aa
Michael Whatcott <mike@smarty.com>
2024-10-02 14:21:48 -0600

    Remove redundant text because it was redundant.

--- code/contracts/error.go
+++ code/contracts/error.go
@@ -7,10 +7,10 @@ import (
 
 func Error(inner error) error {
 	_, file, line, _ := runtime.Caller(1)
-	return fmt.Errorf("error at %s:%d - %w", file, line, inner)
+	return fmt.Errorf("%s:%d - %w", file, line, inner)
 }
 func Errorf(format string, a ...any) error {
 	_, file, line, _ := runtime.Caller(1)
 	inner := fmt.Errorf(format, a...)
-	return fmt.Errorf("error at %s:%d - %w", file, line, inner)
+	return fmt.Errorf("%s:%d - %w", file, line, inner)
 }
